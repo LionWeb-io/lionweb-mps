744ca32dd142ed5d6b5ab8f9626e0476c655ad9c
Niko Stotz <git@nikostotz.de>
2023-03-26 12:27:37 +0200

    added gradle build script

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("com.specificlanguages.mps") version "1.2.0"
+    `maven-publish`
+}
+
+repositories {
+    maven(url = "https://projects.itemis.de/nexus/content/repositories/mbeddr")
+    mavenCentral()
+}
+
+dependencies {
+    "mps"("com.jetbrains:mps:2022.2")
+}
+
+group = "io.lionweb"
+version = "0.0.1"
+
+publishing {
+    publications {
+        register<MavenPublication>("mpsPlugin") {
+            from(components["mps"])
+
+            // Put resolved versions of dependencies into POM files -- uncomment as soon as we have any dependencies
+            // versionMapping { usage("java-runtime") { fromResolutionOf("generation") } }
+        }
+    }
+    repositories {
+        if (project.hasProperty("gpr.user")) {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/specificlanguages/mps-json")
+                credentials {
+                    username = project.findProperty("gpr.user") as String?
+                    password = project.findProperty("gpr.key") as String?
+                }
+            }
+        }
+    }
+}
