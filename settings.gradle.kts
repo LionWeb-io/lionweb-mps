66c7f69b44aa3ee3f1f75ab058c25f3079ab7bf5
Niko <github@nikostotz.de>
2026-02-11 15:04:36 +0100

    pin mps gradle launcher version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,11 +7,13 @@ pluginManagement {
     val netResearchgateReleaseVersion: String by settings
     val comSpecificlanguagesJbrToolchainVersion: String by settings
     val comVanniktechMavenPublishVersion: String by settings
+    val mpsGradleLauncherVersion: String by settings
     plugins {
         id("com.specificlanguages.mps") version comSpecificlanguagesMpsVersion
         id("com.specificlanguages.jbr-toolchain") version comSpecificlanguagesJbrToolchainVersion
         id("net.researchgate.release") version netResearchgateReleaseVersion
         id("com.vanniktech.maven.publish") version comVanniktechMavenPublishVersion
+        id("de.itemis.mps.gradle.launcher") version mpsGradleLauncherVersion
     }
     repositories {
         maven("https://artifacts.itemis.cloud/repository/maven-mps")
